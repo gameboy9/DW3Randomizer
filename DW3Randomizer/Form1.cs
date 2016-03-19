199ca9259e6b3f4eefebe77b6cc53eaeca59419a
Patrick Fifield <plfifield9@yahoo.com>
2016-03-18 22:45:56 -0400

    0.8.4 More bugfixes - Made the intelligence base gain equal to 0 for non-magic users so they can learn magic fairly if they become magic users. - Resolved having duplicate items in weapons or item stores... hopefully... - Made stat gains more fair by only applying the lowest multiplier in one section of stat gains.

--- DW3Randomizer/Form1.cs
+++ DW3Randomizer/Form1.cs
@@ -1124,7 +1124,7 @@ namespace DW3Randomizer
                                       0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
                                       0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f,
                                       0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f,
-                                      0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4e, 0x4f,
+                                      0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4b, 0x4c, 0x4e, 0x4f,
                                       0x50, 0x51, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5c, 0x5d, 0x5e, 0x5f,
                                       0x60, 0x62, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6b, 0x6c, 0x6d, 0x6f,
                                       0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x7b, 0x7c,
@@ -1157,11 +1157,7 @@ namespace DW3Randomizer
                                       0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f,
                                       0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f,
                                       0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46,
-                                      0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4e,
-                                      0x50, 0x52, 0x53, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5f,
-                                      0x60, 0x62, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6c, 0x6d, 0x6f,
-                                      0x70, 0x71, 0x73, 0x74,
-                                      0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4e,
+                                      0x48, 0x49, 0x4b, 0x4c, 0x4e,
                                       0x50, 0x52, 0x53, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5f,
                                       0x60, 0x62, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6c, 0x6d, 0x6f,
                                       0x70, 0x71, 0x73, 0x74,
@@ -1169,6 +1165,25 @@ namespace DW3Randomizer
                                       0x56, 0x65, 0x66, 0x67, 0x68, 0x6c, 0x73, 0x74,
                                       0x56, 0x65, 0x66, 0x67, 0x68, 0x6c, 0x73, 0x74
             };
+            int[] legalStoreWeapons =
+            {
+                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+                                      0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+                                      0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f,
+                                      0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f,
+                                      0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46
+            };
+            int[] legalStoreItems2 =
+            {
+                0x48, 0x49, 0x4b, 0x4c, 0x4e,
+                                      0x50, 0x52, 0x53, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5f,
+                                      0x60, 0x62, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6c, 0x6d, 0x6f,
+                                      0x70, 0x71, 0x73, 0x74,
+                                      0x56, 0x65, 0x66, 0x67, 0x68, 0x6c, 0x73, 0x74,
+                                      0x56, 0x65, 0x66, 0x67, 0x68, 0x6c, 0x73, 0x74,
+                                      0x56, 0x65, 0x66, 0x67, 0x68, 0x6c, 0x73, 0x74
+            };
+
             int[] storeItems = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -1252,6 +1267,35 @@ namespace DW3Randomizer
                 romData[0x36838 + itemStoreLocations[lnI]] += 128;
             }
 
+            // Go through each of the stores and check for duplicates.
+            int startStore = 0;
+            List<int> storeContents = new List<int> { };
+            for (int lnI = 0; lnI < 248; lnI++)
+            {
+                bool lastItem = (romData[0x36838 + lnI] >= 128);
+                int itemToCompare = (romData[0x36838 + lnI] >= 128 ? romData[0x36838 + lnI] - 128 : romData[0x36838 + lnI]);
+                if (storeContents.Contains(itemToCompare))
+                {
+                    // Get new weapon/item.
+                    if (itemToCompare <= 0x48)
+                    {
+                        romData[0x36838 + lnI] = (byte)((lastItem ? 128 : 0) + legalStoreWeapons[r1.Next() % legalStoreWeapons.Length]);
+                    } else
+                    {
+                        romData[0x36838 + lnI] = (byte)((lastItem ? 128 : 0) + legalStoreItems2[r1.Next() % legalStoreItems2.Length]);
+                    }
+                    lnI = startStore - 1;
+                    storeContents.Clear();
+                    continue;
+                }
+                storeContents.Add(itemToCompare);
+                if (lastItem)
+                {
+                    storeContents.Clear();
+                    startStore = lnI + 1;
+                }
+            }
+
             // House of healing cost randomized
             //romData[0x18659] = (byte)(r1.Next() % 20);
 
@@ -1262,10 +1306,6 @@ namespace DW3Randomizer
                 romData[0x367c1 + lnI] -= (byte)(romData[0x367c1 + lnI] % 32);
                 romData[0x367c1 + lnI] += (byte)innPrice;
             }
-
-            // byte[] inns = { 4, 6, 8, 12, 20, 2, 25, 30, 40, 30 };
-            //for (int lnI = 0; lnI < 9; lnI++)
-            //    romData[0x19f90 + lnI] = (byte)((r1.Next() % 20) + 1);
 
             // Verify that key items are available in either a store or a treasure chest in the right zone.
             byte[] keyItems = { 0x59, 0x5a, 0x54, 0x6b, 0x11, 0x78, 0x79, 0x7a, 0x7b, 0x10, 0x75 };
@@ -1323,8 +1363,19 @@ namespace DW3Randomizer
 
             // Randomize stat gains.
             // First, we'll randomize the multipliers.  They will range from 8 to 32, in multiples of 8.
+
+            bool low = false;
             for (int lnI = 0; lnI < 10; lnI++)
+            {
+                if (lnI == 5) low = false;
                 romData[0x281b + lnI] = (byte)(((r1.Next() % 4) + 1) * 8);
+                // Only one low multiplier per five bit segment.
+                if (romData[0x281b + lnI] == 8)
+                    if (low)
+                        lnI--;
+                    else
+                        low = true;
+            }
 
             int[] baseStat = { 3, 2, 4, 3, 2, // Hero
                                1, 3, 2, 3, 2, // Wizard
@@ -1354,12 +1405,14 @@ namespace DW3Randomizer
                 }
                 else // (baseStat[lnI] == 4)
                 {
-                    int[] bp2 = { 1, 2, 3, 4, 4, 4, 4, 4 };
+                    int[] bp2 = { 1, 2, 3, 4, 4, 4, 4, 5, 5, 6 };
                     basePossible = bp2;
                 }
                 newBase = basePossible[r1.Next() % 8];
                 if (lnI >= 16 && lnI < 24 && newBase == 1)
                     newBase++; // Vitality base REALLY shouldn't be 1.  You'll never get HP, and you'll never survive, so we'll just add one to everyone.
+                if (lnI >= 36 && lnI < 40)
+                    newBase = 0; // Give out no intelligence to non-MP users.
                 int charLevel = 0;
                 for (int lnJ = 0; lnJ < 5; lnJ++)
                 {
